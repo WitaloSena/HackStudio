027359be110e415fce47bf95ed47782d82b0bc7a
Witalo Sena <sousawitalo@gmail.com>
2019-05-16 17:44:10 -0300

    Verificar erro de caracteres

--- HackStudio/src/java/br/com/hackstudio/controller/registrar.java
+++ HackStudio/src/java/br/com/hackstudio/controller/registrar.java
@@ -14,9 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author witalo.sena
+ * @author witalo
  */
-public class Controller extends HttpServlet {
+public class registrar extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,7 +29,19 @@ public class Controller extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet registrar</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet registrar at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
